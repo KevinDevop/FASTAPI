ec3915ddae895d794e162b4fdad8e9fc669a8a44
KevinDevop <110274931+KevinDevop@users.noreply.github.com>
2023-02-27 13:40:42 -0500

    Agregamos algunas carpetas al .gitignore

--- jwt.py
+++ jwt.py
@@ -6,8 +6,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 3
 
-def createToken(id_usuario:int, expires_delta : timedelta):
-    to_encode = {"sub": str(id_usuario), "exp": datetime.utcnow() + expires_delta}
+
+def createToken(id_usuario: int, expires_delta: timedelta):
+    to_encode = {"ID": str(id_usuario),
+                 "exp": datetime.utcnow() + expires_delta}
     encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encode_jwt
-
